243c358e228b152740a1ea1d1ec59f24a6368f5c
leandro-msp <leandromarcjt@gmail.com>
2025-09-22 01:00:43 +0000

    Introducao a variaveis

--- aula05_01.php
+++ aula05_01.php
@@ -12,11 +12,11 @@
     $nota=7.5; // a variável nota é tipo float
     $idade=18; // a variável idade é tipo int
     $nome1="Betina";
-    $pessoa_fisica="Salão da Bete"; // metodo um pra dar nome composto para variavel
-    $pessoaFisica="Salão da Bete"; // metodo dois pra dar nome composto para variavel
+    $pessoa_fisica="Salão da Bete"; // método1 pra dar nome composto para variavel.
+    $pessoaFisica="Salão da Bete"; // método2 pra dar nome composto para variavel.
         echo "Seu nome $nome<Br>"; // para mostrar o conteúdo da variável, basta somente inserir o nome da varíavel definida dentro das aspas.
         echo"Sua nota final foi $nota, e sua idade é $idade<br>";
-    // var_dump($nome1); // elemento de teste para ver o tipo da variavel e seus dados
+    // var_dump($nome1); // elemento de teste para ver o tipo da variavel e seus dados.
 
     ?>
 </body>
